121de031918c44d50dd90055bad5d85863369030
Samantha Malone <sammac99@gmail.com>
2021-09-13 17:39:05 -0700

    added queries

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,8 +3,36 @@ const { gql } = require("apollo-server-express");
 
 // create our typeDefs
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
+
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
   type Query {
-    thoughts: # what goes here?
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
   }
 `;
 
--- server/server.js
+++ server/server.js
@@ -6,15 +6,19 @@ const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-// create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-// integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
@@ -23,7 +27,6 @@ app.use(express.json());
 db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-    // log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
